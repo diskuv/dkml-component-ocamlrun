da5ccd82515bb043707a9c4aabd1d4f2493b669c
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-02 22:04:26 -0800

    Add pins for offline-unixutils

--- .github/workflows/scripts/pin-unix-asset.sh
+++ .github/workflows/scripts/pin-unix-asset.sh
@@ -6,8 +6,10 @@ opamrun pin dkml-install                         'https://github.com/diskuv/dkml
 opamrun pin dkml-install-runner                  'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
 opamrun pin dkml-package-console                 'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
 opamrun pin dkml-component-staging-curl          'https://github.com/diskuv/dkml-component-curl.git#main' --no-action --yes
+opamrun pin dkml-component-common-unixutils      'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 opamrun pin dkml-component-staging-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 opamrun pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
+opamrun pin dkml-component-offline-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 
 # GitHub stalls with:
 #   <><> Error report <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
--- .github/workflows/scripts/pin-windows-asset.ps1
+++ .github/workflows/scripts/pin-windows-asset.ps1
@@ -3,8 +3,10 @@ opam pin dkml-install                         'https://github.com/diskuv/dkml-in
 opam pin dkml-install-runner                  'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
 opam pin dkml-package-console                 'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
 opam pin dkml-component-staging-curl          'https://github.com/diskuv/dkml-component-curl.git#main' --no-action --yes
+opam pin dkml-component-common-unixutils      'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 opam pin dkml-component-staging-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 opam pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
+opam pin dkml-component-offline-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 
 # until published in fdopen Opam repository
 opam pin diskuvbox 'https://github.com/diskuv/diskuvbox.git#main' --no-action --yes
